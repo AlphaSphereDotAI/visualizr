58fa5f77db9df3bcb10d4083799952b0e001d123
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-09-02 00:20:07 +0300

    feat: add video generation test in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@
 
 from os import getenv
 
-from gradio_client import Client
+from gradio_client import Client, handle_file
 
 
 def test_app() -> None:
@@ -14,3 +14,26 @@ def test_app() -> None:
     """
     client = Client(getenv("VISUALIZR_URL", "http://localhost:7860/"))
     assert client.heartbeat.is_alive()
+
+
+def test_generate_video() -> None:
+    client = Client("http://127.0.0.1:7860/")
+    result = client.predict(
+        infer_type="hubert_audio_only",
+        image_path=handle_file(
+            "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png"
+        ),
+        audio_path=handle_file(
+            "https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav"
+        ),
+        face_sr=False,
+        pose_yaw=0,
+        pose_pitch=0,
+        pose_roll=0,
+        face_location=0.5,
+        face_scale=0.5,
+        step_t=50,
+        seed=0,
+        api_name="/generate_video",
+    )
+    print(result)
